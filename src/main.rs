ad904eba1faf43b031466d4fbea2d5ce461734b2
Alberto Segura <alberto.segura.delgado@gmail.com>
2022-08-11 10:21:08 +0200

    add file extension filter, show matched rules

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ enum ProcessedFile {
     Done(),
 }
 
+fn parse_file_extension_filter(extensions: &str) -> Vec<String> {
+    extensions.split(",").map(str::to_string).collect()
+}
+
 fn compile_rules_to_vec(rules: &str) -> Result<Vec<u8>, &str> {
     let mut compiler = yara::Compiler::new().unwrap();
     compiler = compiler.add_rules_file(rules).unwrap();
@@ -31,23 +35,29 @@ fn load_rules_from_vec(rules: Vec<u8>) -> Result<yara::Rules, &'static str> {
     }
 }
 
-fn process_apk(file_path: &str, rules: Vec<u8>) {
+fn process_apk(file_path: &str, rules: Vec<u8>, extension_filters: Vec<String>) {
     let fname = std::path::Path::new(file_path);
     if let Ok(apk_file) = std::fs::File::open(file_path) {
         if let Ok(mut archive) = zip::ZipArchive::new(apk_file) {
             for i in 0..archive.len() {
-                let mut is_dex = false;
-                {
+                let match_extension: bool = extension_filters[0].eq("*") || {
                     let com_file = archive.by_index_raw(i).unwrap();
-                    is_dex = com_file.name().ends_with(".dex");
-                }
-                if is_dex {
+                    let mut found = false;
+                    for ex in extension_filters.iter() {
+                        if com_file.name().ends_with(&format!(".{}", ex)) {
+                            found = true; break;
+                        }
+                    }
+                    found
+                };
+                if match_extension {
                     let mut decom_file = archive.by_index(i).unwrap();
                     let mut file_buff = Vec::with_capacity(1000000 * 20);
                     decom_file.read_to_end(&mut file_buff);
                     if let Ok(com_rules) = load_rules_from_vec(rules.clone()) {
                         if let Ok(matches) = com_rules.scan_mem(&file_buff, 60) {
-                            println!("{} in {} -> Matches: {}", decom_file.name(), file_path, matches.len());
+                            let matched_rules: Vec<&str> = matches.iter().map(|r| r.identifier).collect();
+                            println!("{} in {} -> Matches: {}. Rules: {:?}.", decom_file.name(), file_path, matches.len(), matched_rules);
                         }
                     } else {
                         println!("Error: Unable to compile rules!");
@@ -62,7 +72,7 @@ fn process_apk(file_path: &str, rules: Vec<u8>) {
     }
 }
 
-fn process_folder(path: &str, threads: usize, rules: Vec<u8>) {
+fn process_folder(path: &str, threads: usize, rules: Vec<u8>, extension_filters: Vec<String>) {
     let pool = threadpool::ThreadPool::new(threads);
     let (tx, rx) = std::sync::mpsc::channel::<ProcessedFile>();
     let mut file_counter = 0;
@@ -74,8 +84,9 @@ fn process_folder(path: &str, threads: usize, rules: Vec<u8>) {
                 let entry_path = entry.path().to_str().unwrap().to_string();
                 let tx = tx.clone();
                 let rls = rules.clone();
+                let ext_fil = extension_filters.clone();
                 pool.execute(move|| {
-                    process_apk(&entry_path, rls);
+                    process_apk(&entry_path, rls, ext_fil);
                     tx.send(ProcessedFile::Done());
                 });
                 file_counter += 1;
@@ -123,9 +134,17 @@ fn main() {
                     .help("Threads")
                     .required(false)
                     .takes_value(true))
+                .arg(clap::Arg::with_name("extensions")
+                    .short("x")
+                    .help("File extensions inside APK to check rules (ej: -x dex,so,png). Use -x \"*\" to scan all files inside APK")
+                    .required(false)
+                    .takes_value(true))
                 .get_matches();
 
     let threads: usize = args.value_of("threads").unwrap_or("4").parse().unwrap();
+    let extensions: String = args.value_of("extensions").unwrap_or("dex,so").parse().unwrap();
+
+    let extension_filters = parse_file_extension_filter(&extensions);
 
     let mut rules: Option<Vec<u8>> = None;
     if let Some(rules_file) = args.value_of("rules") {
@@ -141,7 +160,7 @@ fn main() {
     if let Some(path) = args.value_of("path") {
         println!("Processing files in {}", path);
         if let Some(rls) = rules {
-            process_folder(path, threads, rls);
+            process_folder(path, threads, rls, extension_filters);
         }
     } else {
         println!("ERROR: You must provide path to folder with files to check using '-p' argument!");
